0460ec61d361ea0e35b8b40e0d17c8278bff59f3
NikitaNavosha <Nn.98@mail.ru>
2022-01-21 19:20:20 +0300

    Домашняя работа №1 выполнена!

--- CurseOne/main.swift
+++ CurseOne/main.swift
@@ -1,57 +1,49 @@
-//
-//  main.swift
-//  CurseOne
-//
-//  Created by admin on 20.01.22.
-//
+
 
 import Foundation
 
-print("Hello, World!")
+let a = 10.0
+let b = 14.0
+let c = -20.0
+var x1 = 0.0
+var x2 = 0.0
 
-//1. Написать функцию, которая определяет, четное число или нет
-let numberFunction  = [4, 7, 14, 21, 28, 63]
-for number in numberFunction {
-if number % 2 == 0 {
-print("\(number) четное число")
-    } else {
-print("\(number) нечетное число")
-    }
+let discriminant = b * b - 4 * a * c
+
+if discriminant < 0 {
+    print("Корней нет")
+} else if discriminant == 0 {
+    x1 = -b/(2 * a)
+} else {
+    x1 = (-b - sqrt(discriminant))/(2 * a)
+    x2 = (-b + sqrt(discriminant))/(2 * a)
 }
-//2. Написать функцию, которая определит делится ли число без остатка на 3.
-for divideThree in numberFunction {
-if divideThree % 3 == 0 {
-print("\(divideThree) делится на 3 без остатка")
-    } else {
-print("\(divideThree) делится на 3 с остатком")
-    }
-}
-//3. Создать возврастающий массив из 100 чисел.
-var oneHundredNumber: Array<Int> = []
-for number in 1...100{
-    oneHundredNumber.append(number)
-}
-print(oneHundredNumber)
-//4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
-var toDelete = oneHundredNumber.filter {$0 % 2 != 0 && $0 % 3 == 0}
-print (toDelete)
-//5. Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 100 элементов.
-func fibonacciArray(_ n: Int) -> [Double] {
-var fibonacci: [Double] = [1, 1]
-    (2...n).forEach{ i in
-        fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
-    }
-return fibonacci
-}
-print(fibonacciArray(100))
-//6. Заполнить массив из 100 элементов различными простыми числами. Натуральное число, большее единицы, называется простым, если оно делится только на себя и на единицу.
-func primeNumbers(arrayPassed: [Int]) -> [Int] {
-var primes: [Int] = []
-var newArr = arrayPassed
-while let newP = newArr.first {
-        primes.append(newP)
-        newArr = newArr.filter { $0 % newP != 0 }
-    }
-return primes
-}
-print(primeNumbers(arrayPassed: Array(2...100)))
+print(discriminant)
+print(x1)
+print(x2)
+
+//2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+let cathet1 = 10
+let cathet2 = 20
+
+let hypotenuse = sqrt((pow(cathet1, 2)) + (pow(cathet2, 2)))
+let yardage = (1/2)*(cathet1 * cathet2)
+let perimeter = cathet1 + cathet2 + hypotenuse
+print (yardage)
+print (perimeter)
+print (hypotenuse)
+
+
+//3. *Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
+let summa:Double = 10000
+let percent:Double = 12.5
+let years:Double = 5
+let daysYear:Double = 365 * years
+
+let end = ( summa * percent * (years/daysYear))/100
+let summaEnd = summa + end
+
+print (end)
+print(summaEnd)
+
+
