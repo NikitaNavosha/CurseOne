a1d2d77861d0bb31f7cd30b88d21753adfbdde9c
NikitaNavosha <Nn.98@mail.ru>
2022-03-13 14:19:29 +0300

    Домашняя работа №5

--- CurseOne/main.swift
+++ CurseOne/main.swift
@@ -2,56 +2,68 @@
 //  main.swift
 //  CurseOne
 //
-//  Created by admin on 20.01.22.
+//  Created by admin on 23.02.22.
 //
 
 import Foundation
 
-enum engineState {
-    case start
-    case stop
+enum windows{
+    case open
+    case closed
 }
-enum trunkState{
+enum engine{
+    case start
+    case off
+}
+enum gas{
+    case gasOn
+    case gasOff
+}
+enum trunk{
     case trunkIsFull
     case TrunkIsEmpty
 }
-class Car{
+protocol Car {
+    var brand: String {get}
+    var release: Int {get}
+    var engineState: engine {get set}
+    var windowsState: windows {get set}
+}
+extension Car {
+    mutating func changeEngineState(engineState: engine){
+        self.engineState = engineState
+    }
+    mutating func chageWindowsState(windowsState: windows){
+        self.windowsState = windowsState
+    }
+}
+class basicCar: Car{
     var brand: String
     var release: Int
-    init(brand: String, release: Int){
+    var engineState: engine = .off
+    var windowsState: windows = .closed
+    init(brand: String, release: Int) {
         self.brand = brand
         self.release = release
     }
 }
-class trunkCar: Car{
-    var trunk: trunkState
-    init(brand: String, release: Int, trunk: trunkState){
-        self.trunk = trunk
-        super.init(brand: brand, release: release)
-    }
-    func trunkState(){
-        if trunk == .trunkIsFull {
-            print("Багажник полон")
-        } else{
-            print("Багажник пуст")
-        }
+class sportCar: basicCar, CustomStringConvertible{
+    var gasState: gas = .gasOff
+    var description: String{
+        return "engine is \(engineState), windows is \(windowsState), gas is \(gasState), release date is \(release)"
     }
 }
-class sportCar: Car{
-    var goFast: engineState
-    init(brand: String, release: Int, goFast: engineState){
-        self.goFast = goFast
-        super.init(brand: brand, release: release)
-    }
-    func engineState(){
-        if goFast == .start {
-            print("Спортивная машина едет")
-        } else{
-            print("Спортивная машина не едет")
-        }
+class trunkCar: basicCar, CustomStringConvertible{
+    var trunkState: trunk = .TrunkIsEmpty
+    var description: String{
+        return "engine is \(engineState), windows is \(windowsState), trunk is \(trunkState), release date is \(release)"
     }
 }
-let gazel = trunkCar(brand: "Lamborgini", release: 2033, trunk: .trunkIsFull)
-gazel.trunkState()
-let mitsubisi = sportCar(brand: "Lada", release: 2001, goFast: .start)
-mitsubisi.engineState()
+var lamborigni = sportCar(brand: "Lamborgini", release: 2033)
+var lada = trunkCar(brand: "Lada", release: 2001)
+lamborigni.changeEngineState(engineState: .start)
+lamborigni.gasState = .gasOn
+lada.chageWindowsState(windowsState: .open)
+lada.trunkState = .trunkIsFull
+print(lamborigni.description)
+print(lada.description)
